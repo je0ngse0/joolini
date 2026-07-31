3695d5b6f09bb295a0b89af63d0ad0c1e0f482c1
임정서 <chzhzmzl465@nate.com>
2026-07-31 11:13:26 +0900

    first commit

---
+++ src/main/java/com/example/joolini/JooliniApplication.java
@@ -0,0 +1,13 @@
+package com.example.joolini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooliniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooliniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joolini/JooliniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joolini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooliniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
